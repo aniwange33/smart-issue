06b7dd0f808aac88037812493dfff6c987b1818c
Aniwange Tertese Amos <terteseamos@gmail.com>
2025-04-30 15:13:41 +0100

    feat: initial setup with requirement document

---
+++ src/main/java/com/amos/silog/SmartTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.amos.silog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amos/silog/SmartTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos.silog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
